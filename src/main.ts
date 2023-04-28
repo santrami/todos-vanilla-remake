785edacdfc81c84815b91af2630a865b835adc00
santrami <santrami@hotmail.com>
2023-04-28 21:17:31 +0200

    checking checkbox on li clic

--- src/main.ts
+++ src/main.ts
@@ -15,30 +15,37 @@ const render = () => {
   for (const todoItem of todoList) {
     const item = document.createElement("li");
     const checkbox = document.createElement("input");
-    checkbox.type="checkbox";
-    checkbox.checked = todoItem.hecha;
-    item.appendChild(checkbox)
+    checkbox.type = "checkbox";
+
+    item.addEventListener('click', ()=>{
+      checkbox.checked = !checkbox.checked;
+      todoItem.hecha = checkbox.checked;
+      console.log(todoList);
+
+    });
+    item.appendChild(checkbox);
     const tareaElem = document.createElement("span");
     tareaElem.textContent = todoItem.tarea;
-    item.appendChild(tareaElem)
+    item.appendChild(tareaElem);
     orderedList?.appendChild(item);
   }
 };
 
-
 const nuevoItem = () => {
-  const input = document.querySelector<HTMLInputElement>("#texto")!;//admiración es para indicar que el input existe, ya que podría ser null y typescript me da error. Es decir, si quito la admiración, los input referenciados más abajo saltan con error
+  const input = document.querySelector<HTMLInputElement>("#texto")!; //admiración es para indicar que el input existe, ya que podría ser null y typescript me da error. Es decir, si quito la admiración, los input referenciados más abajo saltan con error
   todoList.push({
     tarea: input.value,
     hecha: false,
   });
-  console.log(todoList);
   input.value = "";
   render();
-}
+};
 
-let form = document.querySelector<HTMLFormElement>("form");
-form?.addEventListener("submit", (e:Event)=>{
-  e.preventDefault();
-  nuevoItem();
-});
+const programarEventos = () => {
+  let form = document.querySelector<HTMLFormElement>("form");
+  form?.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    nuevoItem();
+  });
+};
+document.addEventListener("DOMContentLoaded", programarEventos);
