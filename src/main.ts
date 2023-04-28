01fc001d733e3091b7b32cdf011d5859e8439d81
santrami <santrami@hotmail.com>
2023-04-28 14:35:05 +0200

    agregando input check y estilos; añandiendo funcion

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,38 @@ type Todoitem = {
 
 const todoList: Array<Todoitem> = [];
 
+// generate DOM structure for visualization of the model
 const render = () => {
-  // generate DOM structure for visualization of the model
   const orderedList = document.querySelector<HTMLOListElement>("#todo-list")!;
   orderedList.textContent = null; // vaciar DOM element
 
   for (const todoItem of todoList) {
     const item = document.createElement("li");
-    item.textContent = todoItem.tarea;
+    const checkbox = document.createElement("input");
+    checkbox.type="checkbox";
+    checkbox.checked = todoItem.hecha;
+    item.appendChild(checkbox)
+    const tareaElem = document.createElement("span");
+    tareaElem.textContent = todoItem.tarea;
+    item.appendChild(tareaElem)
     orderedList?.appendChild(item);
   }
 };
 
-let form = document.querySelector<HTMLFormElement>("form");
 
-form?.addEventListener("submit", (e: Event) => {
-  e.preventDefault();
-  const input = document.querySelector<HTMLInputElement>("#texto")!;
+const nuevoItem = () => {
+  const input = document.querySelector<HTMLInputElement>("#texto")!;//admiración es para indicar que el input existe, ya que podría ser null y typescript me da error. Es decir, si quito la admiración, los input referenciados más abajo saltan con error
   todoList.push({
     tarea: input.value,
     hecha: false,
   });
+  console.log(todoList);
   input.value = "";
   render();
+}
+
+let form = document.querySelector<HTMLFormElement>("form");
+form?.addEventListener("submit", (e:Event)=>{
+  e.preventDefault();
+  nuevoItem();
 });
